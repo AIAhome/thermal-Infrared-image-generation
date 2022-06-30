7edd0e00bc0c3f7e3ccdc78efd3d2bc6c3cc517f
twjia <645014521@qq.com>
2022-06-30 11:27:34 +0800

    discogan plot

--- discogan/run.sh
+++ discogan/run.sh
@@ -1,3 +1,3 @@
 CUDA_VISIBLE_DEVICES=5 nohup python3 -u discogan.py \
---n_epochs=101  --batch_size=8 --checkpoint_interval=10 --sample_interval=400 --lambda_perceptual=0.05  --b_channels=3 \
+--n_epochs=121  --batch_size=8 --checkpoint_interval=10 --sample_interval=400 --lambda_perceptual=0.05 --b_channels=3 \
 > discogan_vgg_final.log &
